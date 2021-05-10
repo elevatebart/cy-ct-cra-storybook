1e974493763cf809052af7aa464b54b96a65c615
ElevateBart <ledouxb@gmail.com>
2021-05-10 14:09:35 -0500

    add preview to the support

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,26 +1,27 @@
-import React from 'react';
+import React from "react";
 
 export const decorators = [
-  (StoryFn, { globals: { locale = 'en' } }) => (
-    <>
-      <div>{locale}</div>
-      <StoryFn />
-    </>
-  ),
+  (StoryFn, { globals: { locale = "en" } }) =>
+    React.Fragment({
+      children: [
+        React.createElement("div", { children: [locale] }),
+        React.createElement(StoryFn()),
+      ],
+    }),
 ];
 
 export const globalTypes = {
   locale: {
-    name: 'Locale',
-    description: 'Internationalization locale',
-    defaultValue: 'en',
+    name: "Locale",
+    description: "Internationalization locale",
+    defaultValue: "en",
     toolbar: {
-      icon: 'globe',
+      icon: "globe",
       items: [
-        { value: 'en', right: '🇺🇸', title: 'English' },
-        { value: 'es', right: '🇪🇸', title: 'Español' },
-        { value: 'zh', right: '🇨🇳', title: '中文' },
-        { value: 'kr', right: '🇰🇷', title: '한국어' },
+        { value: "en", right: "🇺🇸", title: "English" },
+        { value: "es", right: "🇪🇸", title: "Español" },
+        { value: "zh", right: "🇨🇳", title: "中文" },
+        { value: "kr", right: "🇰🇷", title: "한국어" },
       ],
     },
   },
--- cypress/support/index.js
+++ cypress/support/index.js
@@ -12,9 +12,13 @@
 // You can read more here:
 // https://on.cypress.io/configuration
 // ***********************************************************
+import { setGlobalConfig } from "@storybook/testing-react";
+import * as sbPreview from "../../.storybook/preview";
+
+// setGlobalConfig(sbPreview);
 
 // Import commands.js using ES2015 syntax:
-import './commands'
+import "./commands";
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
