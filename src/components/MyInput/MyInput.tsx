f5136f2758ca70b5daf107c313673b38a1b62678
ElevateBart <ledouxb@gmail.com>
2021-05-12 16:21:42 -0500

    add svg to input

--- src/components/MyInput/MyInput.tsx
+++ src/components/MyInput/MyInput.tsx
@@ -4,10 +4,14 @@ export interface MyInputProps {
   value?: string;
 }
 
-const MyInput: React.FC<MyInputProps> = ({ value }) => (
-  <div>
-    <input value={value} />
-  </div>
-);
+const MyInput: React.FC<MyInputProps> = ({ value }) => {
+  const [val, setVal] = React.useState(value);
+  return (
+    <div>
+      <input value={val} onChange={(e) => setVal(e.target.value)} />
+      {val ? <svg>X</svg> : undefined}
+    </div>
+  );
+};
 
 export default MyInput;
